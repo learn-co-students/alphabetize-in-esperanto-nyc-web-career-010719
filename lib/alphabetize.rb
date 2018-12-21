66f0e5e44b79f786830a342bca2ffc0c6be7f7aa
github username <github email address>
2018-12-21 00:11:01 -0500

    alphabet solution

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,6 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
   # code here
+  arr.sort_by { |word| word.split("").map { |letter| ESPERANTO_ALPHABET.index(letter) } }
 end
